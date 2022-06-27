b9569ac707ec39d9feb55a0f303d5bfb7123697a
damar <damar_aji@linkaja.id>
2022-06-27 20:10:13 +0700

    Adding upload list

--- dashboard/upload.php
+++ dashboard/upload.php
@@ -1,26 +1,44 @@
+
+<link rel="icon" href="./favicon.ico" type="image/x-icon">
+<title>Caricapps - Upload</title>
+
+<style>
+        #loader1 {
+            position:absolute;
+            left:40%;
+            top:35%;
+            border-radius:20px;
+            padding:25px;
+        }
+    </style>
+        <script>
+        function hideLoader() { 
+            document.getElementById('loader1').style.display = "none"; 
+        }
+    </script>
 <form method="POST" action="upload.php" id="profileData" enctype="multipart/form-data">
   <div class="form-row">
     <div class="col">
       <div class="form-group">
-        <label for="firstName">First Name</label>
-        <input name="firstName" type="text" id="firstName" class="form-control" placeholder="First name">
-      </div>
-    </div>
-    <div class="col">
-      <div class="form-group">
-        <label for="lastName">Last Name</label>
-        <input name="lastName" type="text" id="lastName" class="form-control" placeholder="Last name">
+        <label for="catatan">Catatan</label>
+        <input name="catatan" type="text" id="catatan" class="form-control" placeholder="Catatan">
       </div>
     </div>
   </div>
   <div class="form-group">
-    <label for="sendimage">Profile Picture</label>
+    <label for="sendimage">Upload</label>
     <input name="sendimage" type="file" class="form-control" id="sendimage">
   </div>
   <div class="form-group">
     <button type="button" id="submit" class="btn btn-outline-primary">Submit</button>
   </div>
 </form>
+<input type="button" value="List" onClick="myFunction()"/> 
+     <script>
+       function myFunction() {
+         window.location.href="./upload_list.php";  
+       }
+     </script>
 <script>
 let host = "http://caricapps.herokuapp.com";
 
@@ -38,5 +56,7 @@ button.addEventListener('click', () => {
   fetch(request)
     .then(response => response.json())
     // .then(data => { console.log(data); })
+
+document.getElementById("profileData").reset();
 });
 </script>
